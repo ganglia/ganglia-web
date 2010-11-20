5c188d34660b30a3812cb2e39740fb755380f81e
vvuksan <vlemp@vuksan.com>
2010-11-19 20:09:28 -0500

    Eliminate hostname as a metric

--- search.php
+++ search.php
@@ -8,24 +8,29 @@ require_once('./cache.php');
 $query = $_GET['q'];
 $results = "";
 
-// First we look for the hosts
-foreach ( $index_array['hosts'] as $key => $host_name ) {
-  if ( preg_match("/$query/", $host_name ) ) {
-    $cluster_name = $index_array['cluster'][$host_name];
-    $results .= "Host: <a target=\"_blank\" href=\"?c=" . $cluster_name . "&h=" . $host_name . "&m=cpu_report&r=hour&s=descending&hc=4&mc=2\">" . $host_name . "</a><br>";
-  }
-}
+if ( $query != "" ) {
 
-// Now let's look through metrics.
-foreach ( $index_array['metrics'] as $metric_name => $hosts ) {
-  if ( preg_match("/$query/", $metric_name ) ) {
-    $cluster_name = $index_array['cluster'][$host_name];
-    foreach ( $hosts as $key => $host_name ) {
-      $results .= "Metric: <a target=\"_blank\" href=\"?c=" . $cluster_name . "&h=" . $host_name . "&m=cpu_report&r=hour&s=descending&hc=4&mc=2#metric_" . $metric_name  . "\">" . $host_name . " (" . $metric_name .  " )</a><br>";
+  // First we look for the hosts
+  foreach ( $index_array['hosts'] as $key => $host_name ) {
+    if ( preg_match("/$query/", $host_name ) ) {
+      $cluster_name = $index_array['cluster'][$host_name];
+      $results .= "Host: <a target=\"_blank\" href=\"?c=" . $cluster_name . "&h=" . $host_name . "&m=cpu_report&r=hour&s=descending&hc=4&mc=2\">" . $host_name . "</a><br>";
     }
   }
-}
 
+  // Now let's look through metrics.
+  foreach ( $index_array['metrics'] as $metric_name => $hosts ) {
+    if ( preg_match("/$query/", $metric_name ) ) {
+      $cluster_name = $index_array['cluster'][$host_name];
+      foreach ( $hosts as $key => $host_name ) {
+	$results .= "Metric: <a target=\"_blank\" href=\"?c=" . $cluster_name . "&h=" . $host_name . "&m=cpu_report&r=hour&s=descending&hc=4&mc=2#metric_" . $metric_name  . "\">" . $host_name . " (" . $metric_name .  " )</a><br>";
+      }
+    }
+  }
+
+} else {
+  $results .= "Empty query string";
+}
 
 if ( $results == "" ) {
   print "No results. Try a different search term. Currently only one search term supported.";
