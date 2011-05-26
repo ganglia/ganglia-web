b45586bed5dca5e60b8320b0a260d781bed2f29f
Peter Piela <peter.piela@terascala.com>
2011-05-26 12:39:13 -0400

    Removed reference to storeAggregateGraph function. Sync'd with upstream master

--- ganglia-web/aggregate_graphs.php
+++ ganglia-web/aggregate_graphs.php
@@ -4,66 +4,60 @@
 }
 </style>
 <script>
-$("#hreg").change(function() {
-  $.cookie("ganglia-aggregate-graph-hreg" + window.name,
-	   $("#hreg").val());
-});
-
-$("#aggregate_graph_table_form input[name=gtype]").change(function() {
-  $.cookie("ganglia-aggregate-graph-gtype" + window.name,
-	   $("#aggregate_graph_table_form input[name=gtype]:checked").val());
-});
-
-function restoreAggregateGraph() {
-  var hreg = $.cookie("ganglia-aggregate-graph-hreg" + window.name);
-  if (hreg != null)
-    $("#hreg").val(hreg);
-  
-  var gtype = $.cookie("ganglia-aggregate-graph-gtype" + window.name);
-  if (gtype != null)
-    $("#aggregate_graph_table_form input[name=gtype]").val([gtype]);
-  
-  var metric = $.cookie("ganglia-aggregate-graph-metric" + window.name);
-  if (metric != null)
-    $("#metric_chooser").val(metric);
-  
-  if (hreg != null && gtype != null && metric != null)
-    return true;
-  else
-    return false;
-}
-
-function createAggregateGraph() {
-  $("#aggregate_graph_display").html('<img src="img/spinner.gif">');
-  $.get('graph_all_periods.php', $("#aggregate_graph_form").serialize() + "&aggregate=1" , function(data) {
-    $("#aggregate_graph_display").html(data);
-  });
-  return false;
-}
-
-var availableMetrics = [ <?php
-  require_once('./cache.php');
-  foreach ( $index_array['metrics'] as $key => $value)
-    $metrics[] = "'" . $key . "'";
-
-  print join(',', $metrics);
-  ?>
-];
-
 $(function() {
-    $( "#metric_chooser" ).autocomplete({
-      source: availableMetrics,
+
+  $( ".ag_buttons" ).button();
+
+  $("#hreg").change(function() {
+    $.cookie("ganglia-aggregate-graph-hreg" + window.name,
+      $("#hreg").val());
+  });
+
+  $("#aggregate_graph_table_form input[name=gtype]").change(function() {
+    $.cookie("ganglia-aggregate-graph-gtype" + window.name,
+	   $("#aggregate_graph_table_form input[name=gtype]:checked").val());
+  });
+
+  function restoreAggregateGraph() {
+    var hreg = $.cookie("ganglia-aggregate-graph-hreg" + window.name);
+    if (hreg != null)
+      $("#hreg").val(hreg);
+  
+    var gtype = $.cookie("ganglia-aggregate-graph-gtype" + window.name);
+    if (gtype != null)
+      $("#aggregate_graph_table_form input[name=gtype]").val([gtype]);
+  
+    var metric = $.cookie("ganglia-aggregate-graph-metric" + window.name);
+    if (metric != null)
+      $("#metric_chooser").val(metric);
+  
+    if (hreg != null && gtype != null && metric != null)
+      return true;
+    else
+      return false;
+  }
+
+  function createAggregateGraph() {
+    $("#aggregate_graph_display").html('<img src="img/spinner.gif">');
+    $.get('graph_all_periods.php', $("#aggregate_graph_form").serialize() + "&aggregate=1" , function(data) {
+      $("#aggregate_graph_display").html(data);
+    });
+    return false;
+  }
+
+  $( "#metric_chooser" ).autocomplete({
+      source: availablemetrics,
       change: function(event, ui) {
 	$.cookie("ganglia-aggregate-graph-metric" + window.name,
 	         $("#metric_chooser").val());
       }
-    });
-  $( ".ag_buttons" ).button();
-});
+  });
 
-$(document).ready(function() {
+  $(document).ready(function() {
   if (restoreAggregateGraph())
     createAggregateGraph();
+  });
+
 });
 </script>
 <div id="aggregate_graph_header">
@@ -83,7 +77,8 @@ $(document).ready(function() {
 <div id="graph_type_menu"><input type="radio" name="gtype" value="line" checked>Line</input>
 <input type="radio" name="gtype" value="stack">Stacked</input></div></td>
 <td>
-<button class="ag_buttons" onclick="storeAggregateGraph();createAggregateGraph(); return false">Create Graph</button></td>
+<button class="ag_buttons" onclick="createAggregateGraph(); return false">Create Graph</button></td>
+<button class="ag_buttons" onclick="metricActionsAggregateGraph($('#hreg').val(), $('#metric_chooser').val(), $('input[name=gtype]:checked').val()); return false">Add To View</button></td>
 </tr>
 </table>
 </form>
