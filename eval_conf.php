8f98d3e947516e33ec61923bc64d346362582ce4
Jeff Buchbinder <jbuchbinder@ravemobilesafety.com>
2011-06-27 11:09:42 -0400

    Adust error handling in events API to consistently return valid JSON. Use PHP error handling mechanism in configuration parsing.

--- eval_conf.php
+++ eval_conf.php
@@ -65,13 +65,13 @@ if( ! isSet( $conf['auth_system'] ) ) {
 }
 
 if( count($errors) ) {
-  echo "<h1>Errors were detected in your configuration.</h1>";
-  echo "<ul class='errors'>";
+  $e = "<h1>Errors were detected in your configuration.</h1>";
+  $e .= "<ul class='errors'>";
   foreach($errors as $error) {
-    echo "<li>$error</li>";
+    $e .= "<li>$error</li>";
   }
-  echo "</ul>";
-  die();
+  $e .= "</ul>";
+  trigger_error( $e, E_USER_ERROR );
 }
 
 # These are settings derived from the configuration settings, and
