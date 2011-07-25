3efe9f4ccdf48875666c9cb0e41430211d11f3dd
vuksan <vuksan@mygpslocation.com>
2011-07-24 21:49:09 -0400

    We don't need livesearch in detached view mode

--- ganglia-web/views.php
+++ ganglia-web/views.php
@@ -189,11 +189,9 @@ if ( sizeof($available_views) == 0 ) {
 <html><head>
 <script TYPE="text/javascript" SRC="js/jquery-1.6.2.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.11.custom.min.js"></script>
-<script type="text/javascript" src="js/jquery.liveSearch.js"></script>
 <script type="text/javascript" src="js/ganglia.js"></script>
 <script type="text/javascript" src="js/jquery.cookie.js"></script>
 <link type="text/css" href="css/smoothness/jquery-ui-1.8.11.custom.css" rel="stylesheet" />
-<link type="text/css" href="css/jquery.liveSearch.css" rel="stylesheet" />
 <LINK rel="stylesheet" href="./styles.css" type="text/css">
 <?php
 if ( isset($_GET['standalone']) && isset($_GET['view_name']) ) {
