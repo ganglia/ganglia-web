000af79126aae464adf1589b39366a2058caf265
vuksan <vuksan@mygpslocation.com>
2011-08-25 10:22:45 -0400

    Show real dates instead of UNIX time in enlarged flot graphs

--- enlarge_graph.php
+++ enlarge_graph.php
@@ -13,6 +13,8 @@
 <script language="javascript" type="text/javascript" src="js/jquery.flot.min.js"></script>
 <script language="javascript" type="text/javascript" src="js/jquery.flot.crosshair.min.js"></script>
 <script type="text/javascript" src="js/create-flot-graphs.js"></script>
+<div class="img_view">
 <div id="placeholder_<?php print $_SERVER['QUERY_STRING']; ?>_legend" class="flotlegend"></div>
 <div id="placeholder_<?php print $_SERVER['QUERY_STRING']; ?>" class="flotgraph-enlarge img_view"></div>
 <p id="hoverdata"></p> 
+</div>
