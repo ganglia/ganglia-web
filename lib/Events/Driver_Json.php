cc7411a782c2dd4e343925e932c558c930e60640
Jeff Buchbinder <jeff@ourexchange.net>
2011-12-17 16:08:05 -0500

    Implement ganglia_event_modify for JSON event driver.

--- lib/Events/Driver_Json.php
+++ lib/Events/Driver_Json.php
@@ -24,4 +24,54 @@ function ganglia_events_get() {
 	return $events_array;
 } // end method ganglia_events_get
 
+function ganglia_event_modify( $event ) {
+	global $conf;
+  $event_found = 0;
+  $events_array = ganglia_events_get();
+  $new_events_array = array();
+
+  if (isset($event['id'])) {
+    api_return_error( "Event ID not found" );
+  } // isset event_id
+
+  foreach ( $events_array AS $k => $e ) {
+    if ( $e['event_id'] == $event['id'] ) {
+      $event_found = 1;
+
+      if (isset( $event['start_time'] )) {
+        if ( $event['start_time'] == "now" ) {
+          $e['start_time'] = time();
+        } else if ( is_numeric($event['start_time']) ) {
+          $e['start_time'] = $event['start_time'];
+        } else {
+          $e['start_time'] = strtotime($event['start_time']);
+        }
+      } // end isset start_time
+
+      foreach(array('cluster', 'description', 'summary', 'grid', 'host_regex') AS $k) {
+        if (isset( $event[$k] )) {
+          $e[$k] = $event[$k];
+        }
+      } // end foreach
+
+      if ( isset($event['end_time']) ) {
+        $e['end_time'] = $event['end_time'] == "now" ? time() : strtotime($event['end_time']);
+      } // end isset end_time
+    } // if event_id
+
+    // Add either original or modified event back in
+    $new_events_array[] = $e;
+  } // foreach events array
+  if ( $event_found == 1 ) {
+    $json = json_encode($new_events_array);
+    if ( file_put_contents($conf['overlay_events_file'], $json) === FALSE ) {
+      api_return_error( "Can't write to file " . $conf['overlay_events_file'] . ". Perhaps permissions are wrong." );
+    } else {
+      $message = array( "status" => "ok", "message" => "Event ID " . $event_id . " removed successfully" );
+    }
+  } // end if event_found
+
+  return $message;
+} // end method ganglia_event_modify
+
 ?>
