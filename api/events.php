c5ee2a23e49510a2a840808f1724fbf6d5b5ffd1
vuksan <vuksan@mygpslocation.com>
2012-01-25 16:50:39 -0500

    Support either GETs or POSTs

--- api/events.php
+++ api/events.php
@@ -23,58 +23,58 @@ if ( ! $conf['overlay_events'] ) {
 
 # If events_auth_token is specified in conf.php use that.
 if ( isset($conf['events_auth_token']) ) {
-   if ( ! ( isset($_GET['token']) && $conf['events_auth_token'] == $_GET['token'] ) ) {
-      api_return_error( "Error: Events Auth Token is invalid. Please check token=" );
+   if ( ! ( isset($_REQUEST['token']) && $conf['events_auth_token'] == $_REQUEST['token'] ) ) {
+      api_return_error( "Error: Events Auth Token is invalid. Please check token" );
    }
 }
 
-if ( !isset($_GET['action']) ) {
+if ( !isset($_REQUEST['action']) ) {
   api_return_error( "Error: You need to specify an action at a minimum" );
 }
 
-switch ( $_GET['action'] ) {
+switch ( $_REQUEST['action'] ) {
  
   case "add":
-    if ( ! isset($_GET['start_time']) || ! isset($_GET['summary']) || ! isset($_GET['host_regex']) ) {
+    if ( ! isset($_REQUEST['start_time']) || ! isset($_REQUEST['summary']) || ! isset($_REQUEST['host_regex']) ) {
       api_return_error( "Error: You need to supply start_time, summary, host_regex at a minimum" );
     }
 
     // If the time is now just insert the current time stamp. Otherwise use strtotime
     // to convert
-    if ( $_GET['start_time'] == "now" )
+    if ( $_REQUEST['start_time'] == "now" )
       $start_time = time();
-    else if ( is_numeric($_GET['start_time']) ) 
-      $start_time = $_GET['start_time'];
+    else if ( is_numeric($_REQUEST['start_time']) ) 
+      $start_time = $_REQUEST['start_time'];
     else 
-      $start_time = strtotime($_GET['start_time']);
+      $start_time = strtotime($_REQUEST['start_time']);
 
-    $grid = isset($_GET['grid']) ? $_GET['grid'] : "*";
-    $cluster = isset($_GET['cluster']) ? $_GET['cluster'] : "*";
-    $description = isset($_GET['description']) ? $_GET['description'] : "";
+    $grid = isset($_REQUEST['grid']) ? $_REQUEST['grid'] : "*";
+    $cluster = isset($_REQUEST['cluster']) ? $_REQUEST['cluster'] : "*";
+    $description = isset($_REQUEST['description']) ? $_REQUEST['description'] : "";
     // Generate a unique event ID. This is so we can reference it later
     $event_id = uniqid();
 
-    $event = array( "event_id" => $event_id, "start_time" => $start_time, "summary" => $_GET['summary'],
-      "grid" => $grid, "cluster" => $cluster, "host_regex" => $_GET['host_regex'],
+    $event = array( "event_id" => $event_id, "start_time" => $start_time, "summary" => $_REQUEST['summary'],
+      "grid" => $grid, "cluster" => $cluster, "host_regex" => $_REQUEST['host_regex'],
       );
 
-    if ( isset($_GET['end_time']) )
-      $event['end_time'] = $_GET['end_time'] == "now" ? time() : strtotime($_GET['end_time']);
+    if ( isset($_REQUEST['end_time']) )
+      $event['end_time'] = $_REQUEST['end_time'] == "now" ? time() : strtotime($_REQUEST['end_time']);
 
     $message = ganglia_events_add( $event );
     break;
 
   case "edit":
-    $message = ganglia_event_modify( $_GET );
+    $message = ganglia_event_modify( $_REQUEST );
     break;
 
   case "remove":
   case "delete":
 
-    if ( !isset( $_GET['event_id'] ) ) {
+    if ( !isset( $_REQUEST['event_id'] ) ) {
       api_return_error( "No event_id has been supplied." );
     }
-    $message = ganglia_event_delete( $_GET['event_id'] );
+    $message = ganglia_event_delete( $_REQUEST['event_id'] );
 
     break;
 
@@ -83,7 +83,7 @@ switch ( $_GET['action'] ) {
     api_return_error( "No valid action specified" );
     break;
 
-} // end of switch ( $_GET['action'] ) {
+} // end of switch ( $_REQUEST['action'] ) {
 
 print json_encode($message);
 
