ae2bd4295298382aaf08f13efec0936b7ddab12e
vuksan <vuksan@mygpslocation.com>
2012-02-25 12:37:41 -0500

    Use better styling on the month chooser

--- trend_navigation.php
+++ trend_navigation.php
@@ -1,10 +1,29 @@
-<center>Show trend months ahead 
+<center>
+<div id="trend_range_menu">Extend trend line 
 <?php
 $months_ahead = array(3,6,9,12,18,24);
+
+if ( !isset($_REQUEST['trendrange']) )
+  $_REQUEST['trendrange'] = 6;
+
 foreach ( $months_ahead as $index => $month ) {
-?>
-   <a href='#' onClick='drawTrendGraph("<?php print $_REQUEST['url'] . "&trendrange=" . $month ?>"); return false;"
-<?php
+
+  if (  $_REQUEST['trendrange'] == $month )
+    $checked = 'checked="checked"';
+  else
+    $checked = "";
 
 ?>
+   <input OnChange='drawTrendGraph("<?php print $_SERVER['QUERY_STRING'] . "&trendrange=" . $month; ?>"); return false;' type="radio" id="range-<?php print $month; ?>" name="r" value="<?php print $month; ?>" <?php print $checked; ?>/>
+   <label for="range-<?php print $month; ?>"><?php print $month; ?></label>
+<?php
+}
+?>
+months ahead</div>
+<br>
 </center>
+<script type="text/javascript">
+  $(function () {
+    $("#trend_range_menu").buttonset();
+  });
+</script>
