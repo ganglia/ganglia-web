860b09682ee4589c31e18ff78b68f01ec7110f5b
vuksan <vuksan@mygpslocation.com>
2012-03-07 10:44:31 -0500

    Add metric cache warmup script

---
+++ nagios/warmup_metric_cache.php
@@ -0,0 +1,31 @@
+<?php
+
+###############################################################################
+# There are race conditions when using metrics cache with Nagios where
+# you can have multiple check script querying gmetad and writing to the 
+# same file. To avoid this condition you can run this script periodically
+# ie. every 10-15 seconds to populate the cache. You will find a shell
+# script in this directory which runs this command on a specific schedule
+###############################################################################
+$conf['gweb_root'] = dirname(dirname(__FILE__));
+
+include_once $conf['gweb_root'] . "/eval_conf.php";
+
+$context = "cluster";
+include_once $conf['ganglia_dir'] . "/functions.php";
+include_once $conf['ganglia_dir'] . "/ganglia.php";
+include_once $conf['ganglia_dir'] . "/get_ganglia.php";
+# Put the serialized metrics into a file
+file_put_contents($conf['nagios_cache_file'], serialize($metrics));
+
+foreach ( $metrics as $host => $host_metrics ) {
+    foreach ( $host_metrics as $name => $attributes ) {
+        $new_metrics[$host][$name]['VAL'] = $metrics[$host][$name]['VAL'];
+        if ( isset($metrics[$host][$name]['UNITS']) ) 
+        $new_metrics[$host][$name]['UNITS'] = $metrics[$host][$name]['UNITS'];
+    }
+}
+
+file_put_contents($conf['nagios_cache_file'], serialize($new_metrics));
+
+?>
