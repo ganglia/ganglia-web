e8ad0a005ae1c77b3f1fc4bc6e7c41550bf36c3a
vuksan <vuksan@mygpslocation.com>
2012-03-08 11:01:16 -0500

    Add UI option to use only x months of data when drawing a trendline

--- trend_navigation.php
+++ trend_navigation.php
@@ -1,8 +1,30 @@
-<center>
-<div id="trend_range_menu"><form>
-  Extend trend line 
 <?php
 $months_ahead = array(1,2,3,6,9,12,18,24);
+$months_back = array(12,9,6,3,2,1);
+if ( !isset($_REQUEST['trendrange']) )
+  $_REQUEST['trendrange'] = 6;
+if ( !isset($_REQUEST['trendhistory']) )
+  $_REQUEST['trendhistory'] = 6;
+
+?>
+<center>
+<div id="trend_range_menu">
+<form id="trend_range_form">
+Use data from last 
+<?php
+foreach ( $months_back as $index => $month ) {
+  if (  $_REQUEST['trendhistory'] == $month )
+    $checked = 'checked="checked"';
+  else
+    $checked = "";
+?>
+   <input OnChange='drawTrendGraph("<?php print $_SERVER['QUERY_STRING'] ?>" + "&" + $("#trend_range_form").serialize()); return false;' type="radio" id="trendhistory-<?php print $month; ?>" name="trendhistory" value="<?php print $month; ?>" <?php print $checked; ?>/>
+   <label for="trendhistory-<?php print $month; ?>"><?php print $month; ?></label>
+<?php
+}
+?>
+months&nbsp;|&nbsp;Extend trend line 
+<?php
 
 if ( !isset($_REQUEST['trendrange']) )
   $_REQUEST['trendrange'] = 6;
@@ -15,7 +37,7 @@ foreach ( $months_ahead as $index => $month ) {
     $checked = "";
 
 ?>
-   <input OnChange='drawTrendGraph("<?php print $_SERVER['QUERY_STRING'] . "&trendrange=" . $month; ?>"); return false;' type="radio" id="range-<?php print $month; ?>" name="r" value="<?php print $month; ?>" <?php print $checked; ?>/>
+   <input OnChange='drawTrendGraph("<?php print $_SERVER['QUERY_STRING'] ?>" + "&" + $("#trend_range_form").serialize()); return false;' type="radio" id="range-<?php print $month; ?>" name="trendrange" value="<?php print $month; ?>" <?php print $checked; ?>/>
    <label for="range-<?php print $month; ?>"><?php print $month; ?></label>
 <?php
 }
