e5ca4e0d77c793ba678021d70ecec8f13cdb5375
Brooks Swinnerton <bswinnerton@gmail.com>
2013-02-01 13:53:53 -0500

    Custom fix for Windows memory used
    from: http://www.mail-archive.com/ganglia-general@lists.sourceforge.net/msg07682.html

--- graph.d/mem_report.php
+++ graph.d/mem_report.php
@@ -165,9 +165,9 @@ function graph_mem_report ( &$rrdtool_graph ) {
     // If metrics like mem_used and mem_shared are not present we are likely not collecting them on this
     // host therefore we should not attempt to build anything and will likely end up with a broken
     // image. To avoid that we'll make an empty image
-    if ( !file_exists("$rrd_dir/mem_used.rrd") && !file_exists("$rrd_dir/mem_shared.rrd") ) 
+    /*if ( !file_exists("$rrd_dir/mem_used.rrd") && !file_exists("$rrd_dir/mem_shared.rrd") ) 
       $rrdtool_graph[ 'series' ] = 'HRULE:1#FFCC33:"No matching metrics detected"';   
-    else
+    else*/
       $rrdtool_graph[ 'series' ] = $series;
 
     return $rrdtool_graph;
