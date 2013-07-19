ganglia/ganglia-web

commit 78bf00ebefe8d276749ee27d0d2e2973d437de9b

Handle properly cluster graphs/metrics that are added to a view. Allow those to have max and min

functions.php
@@ -741,16 +741,21 @@ function get_view_graph_elements($view) {
             $hostname = $item['hostname'];
             $cluster = array_key_exists($hostname, $index_array['cluster']) ?
 	      $index_array['cluster'][$hostname][0] : NULL;
+	      $graph_args_array[] = "h=" . urlencode($hostname);
           } else if (isset($item['cluster'])) {
 	    $hostname = "";
             $cluster = $item['cluster'];
+	    $graph_args_array[] = "c=" . urlencode($cluster);
 	  } else {
             $hostname = "";
             $cluster = "";
 	  }
 
-	  $graph_args_array[] = "h=" . urlencode($hostname);
-	  $graph_args_array[] = "c=" . urlencode($cluster);
+          if (isset($item['upper_limit']))
+            $graph_args_array[] = "x=" .$item['upper_limit'];
+
+	  if (isset($item['lower_limit']))
+	    $graph_args_array[] = "n=" .$item['lower_limit'];
 
 	  if (isset($item['vertical_label']))
 	    $graph_args_array[] = "vl=" . urlencode($item['vertical_label']);

commit 7e0bf88fc5ff09e260dcde78f3d0207104b62eac

regardless of which condition was used to determine the cluster, pass it along to the individual graph calls
Appears to have been a regression from
78bf00ebefe8d276749ee27d0d2e2973d437de9b

functions.php
@@ -745,11 +745,11 @@ function get_view_graph_elements($view) {
           } else if (isset($item['cluster'])) {
 	    $hostname = "";
             $cluster = $item['cluster'];
-	    $graph_args_array[] = "c=" . urlencode($cluster);
 	  } else {
             $hostname = "";
             $cluster = "";
 	  }
+      $graph_args_array[] = "c=" . urlencode($cluster);
 
           if (isset($item['upper_limit']))
             $graph_args_array[] = "x=" .$item['upper_limit'];