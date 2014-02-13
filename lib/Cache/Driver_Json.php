97c805d04377a809f98a65ecdead630e42c47489
Vladimir Vuksan <vlemp@vuksan.com>
2014-02-13 18:24:12 +0000

    Add ability to specify alternate cache indexes to avoid pulling everything in

--- lib/Cache/Driver_Json.php
+++ lib/Cache/Driver_Json.php
@@ -8,11 +8,31 @@ function g_cache_exists() {
 function g_cache_serialize($data) {
 	global $conf;
 	file_put_contents($conf['cachefile'], serialize($data));
+	file_put_contents($conf['cachefile'] . "_cluster_data", serialize($data["cluster"]));	
+	file_put_contents($conf['cachefile'] . "_host_list", serialize($data["hosts"]));	
+	file_put_contents($conf['cachefile'] . "_metric_list", serialize(array_keys($data["metrics"])));	
 } // end function g_cache_serialize
 
-function g_cache_deserialize() {
+function g_cache_deserialize($index) {
 	global $conf;
-	$index_array = unserialize(file_get_contents($conf['cachefile']));
+        $index_array = array();
+	
+        switch ( $index ) {
+
+           case "hosts_and_metrics":
+	       $index_array["cluster"] = unserialize(file_get_contents($conf['cachefile'] . "_cluster_data"));
+	       $index_array["hosts"] = unserialize(file_get_contents($conf['cachefile'] . "_host_list"));
+	       $index_array["metrics"] = unserialize(file_get_contents($conf['cachefile'] . "_metric_list"));
+               break;
+        
+           case "metric_list":
+	       $index_array["metrics"] = unserialize(file_get_contents($conf['cachefile'] . "_" . $index));
+               break;
+
+           default:
+	       $index_array = unserialize(file_get_contents($conf['cachefile']));
+             
+        }
 	return $index_array;
 } // end function g_cache_deserialize
 
